5acea72e795a91b7b6b1166a3418d398fc5497f8
Kyle West <Radmank1@outlook.com>
2025-05-25 22:44:59 +0200

    Cleared out discontinued files.
    Added files for organization:
    -- out of use
    -- Other

--- InventoryManagerDefs.cpp
+++
@@ -1,9 +0,0 @@
-#include "InventoryItem.h"
-
-InventoryItem::InventoryItem(const QString& name,
-    const QString& category,
-    const QString& description,
-    const QString& room,
-    int quantity)
-    : name(name), category(category), description(description), room(room), quantity(quantity) {
-}
